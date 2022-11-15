aab990dced7f413eecd2801006c8c9930255ba1b
Mohammad Ali <112762827+MA-HN@users.noreply.github.com>
2022-11-15 23:54:59 +0330

    open the data sheet

--- XLS_Reader_Writer.py
+++ XLS_Reader_Writer.py
@@ -4,3 +4,10 @@ from openpyxl import Workbook #for writing in spreadsheet
 from openpyxl import load_workbook #for reading from spreadsheet
 
 
+techcrunch = load_workbook(filename='Data.xlsx')
+
+for sheets in techcrunch.sheetnames:
+    data_sheet = techcrunch.active(sheets)
+
+
+
